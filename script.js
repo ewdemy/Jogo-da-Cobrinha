3335393387d2cdf88ebf60f716e46a311941df56
ewdemy <ewdemy@hotmail.com>
2021-02-03 19:27:20 -0300

    Aula 03

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ snake[0] = {
 }
 let direction = "right";
 
-
-
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -22,7 +20,21 @@ function criarCobrina(){
     }
 }
 
+document.addEventListener("keydown", update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrina();
     
@@ -45,5 +57,4 @@ function iniciarJogo(){
   
 }
 
-let jogo = setInterval(iniciarJogo, 100);
-jogo();
+let jogo = setInterval(iniciarJogo, 200);
