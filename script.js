d5b4d2d2206f90e149590b8fb26f19854c6c72ab
ewdemy <ewdemy@hotmail.com>
2021-02-03 19:53:42 -0300

    Aula 04

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -18,6 +22,11 @@ function criarCobrina(){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function criarComida(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener("keydown", update);
@@ -37,6 +46,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrina();
+    criarComida();
     
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -57,4 +67,4 @@ function iniciarJogo(){
   
 }
 
-let jogo = setInterval(iniciarJogo, 200);
+let jogo = setInterval(iniciarJogo, 100);
